0dd5bdba6d2adef091244035cbb8b2f57b344e08
Matt Thomas <thomasoncode@gmail.com>
2018-01-20 10:40:08 -0600

    Improving readability

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const http = require('http');
-const {hostname, port} = require('./src/network-configuration');
+const {hostname,port} = require('./src/network-configuration');
 const productController = require('./src/product-controller');
 
 const server = http.createServer(function (request, response) {
-    const path = request.url.split('/').filter(v => v);
+  const path = request.url.split('/').filter(v => v);
 
-    if (path[0] === 'product') {
-      productController(request, response);
-    } else {
-      response.statusCode = 400;
-      response.setHeader('Content-Type', 'application/json');
-      response.end();
-    }
+  if (path[0] === 'product') {
+    productController(request, response);
+  } else {
+    response.statusCode = 400;
+    response.setHeader('Content-Type', 'application/json');
+    response.end();
+  }
+});
 
-  }).listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

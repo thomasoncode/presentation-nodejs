f202cd907d3c52c6057aee178d35a58c1938b1ea
Matt Thomas <thomasoncode@gmail.com>
2018-02-03 12:56:26 -0600

    Added details to package.json

--- src/post-product.js
+++ src/post-product.js
@@ -1,10 +1,12 @@
+const chalk = require('chalk');
+
 module.exports = function (request, response) {
     let body = [];
     request.on('data', (chunk) => {
       body.push(chunk);
     }).on('end', () => {
       const json = Buffer.concat(body).toString();
-      console.log(json);
+      console.log(console.log(chalk.blue(json)));
       response.statusCode = 201;
       response.end();
     });

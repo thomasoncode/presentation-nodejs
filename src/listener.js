7fcf81278078bc75735e3105bbaa9e647856b0e0
Matt Thomas <thomasoncode@gmail.com>
2018-02-22 21:53:17 -0600

    Code update

--- src/listener.js
+++ src/listener.js
@@ -1,5 +1,12 @@
 const {hostname,port} = require('./network-configuration');
 
+/*
+const networkConfiguration = = require('./network-configuration');
+const hostname = networkConfiguration.hostname;
+const port = networkConfiguration.port;
+*/
+
 module.exports = function(){
     console.log(`Server running at http://${hostname}:${port}/`);
+    // console.log('Server running at http://' + hostname + ':' + port '/')
 };
